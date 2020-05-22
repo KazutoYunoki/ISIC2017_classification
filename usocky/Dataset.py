79915b3f54106ffeaddbd283138c7d4bc1f99984
KazutoYunoki <kazuto233@gmail.com>
2020-05-22 21:48:03 +0900

    ディレクトリの整理をしました。

---
+++ usocky/Dataset.py
@@ -0,0 +1,127 @@
+import torch.utils.data as data
+import pandas as pd
+from PIL import Image
+import torch
+
+import os.path as osp
+from ImageTransfom import ImageTransform
+
+
+# データへのpathリストを作成
+def make_datapath_list(phase="train"):
+
+    # ISICのimage_id と教師データのcsvファイル読み込み
+    training_data_CSV_file = pd.read_csv(
+        "./ISIC-2017_Training_Part3_GroundTruth (1).csv"
+    )
+    val_data_CSV_file = pd.read_csv("./ISIC-2017_Validation_Part3_GroundTruth.csv")
+
+    # csvファイルからimage_idの列を取得
+    train_image_id = []
+    val_image_id = []
+
+    train_image_id = training_data_CSV_file["image_id"]
+    val_image_id = val_data_CSV_file["image_id"]
+
+    # 画像のroot_path
+    train_path = "./ISIC-2017_Training_Data/"
+    val_path = "./ISIC-2017_Validation_Data/"
+
+    path_list = []
+
+    if phase == "train":
+        for i in range(len(train_image_id)):
+            # root_pathとimage_idをくっつくて画像へのパスを作成
+            target_path = osp.join(train_path + train_image_id[i] + ".jpg")
+            path_list.append(target_path)
+
+    if phase == "val":
+        for i in range(len(val_image_id)):
+            target_path = osp.join(val_path + val_image_id[i] + ".jpg")
+            path_list.append(target_path)
+
+    return path_list
+
+
+def create_dataloader(batch_size, train_dataset, val_dataset):
+
+    train_dataloader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True
+    )
+
+    val_dataloader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=True
+    )
+
+    dataloader_dict = {"train": train_dataloader, "val": val_dataloader}
+
+    return dataloader_dict
+
+
+class IsicDataset(data.Dataset):
+    def __init__(self, file_list, transform=None, phase="train"):
+        self.file_list = file_list
+        self.transform = transform
+        self.phase = phase
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, index):
+        img_path = self.file_list[index]
+        img = Image.open(img_path)
+        img_transformed = self.transform(img, self.phase)
+
+        label = []
+
+        # 画像のラベルを読み込み
+        if self.phase == "train":
+            train_data_csv_file = pd.read_csv(
+                "./ISIC-2017_Training_Part3_GroundTruth (1).csv"
+            )
+            train_label = []
+            # melanomaのラベルを取得
+            train_label = train_data_csv_file["melanoma"]
+            # labelのデータ型をfloat→int64
+            train_label = train_label.astype("int64")
+            # index番目のラベルを取得
+            label = train_label[index]
+
+        elif self.phase == "val":
+            val_data_csv_file = pd.read_csv(
+                "./ISIC-2017_Validation_Part3_GroundTruth.csv"
+            )
+            val_label = []
+            val_label = val_data_csv_file["melanoma"]
+            val_label = val_label.astype("int64")
+            label = val_label[index]
+
+        return img_transformed, label
+
+
+# 動作確認
+if __name__ == "__main__":
+
+    train_list = make_datapath_list(phase="train")
+    val_list = make_datapath_list(phase="val")
+    # 画像へのパスがきちんと通っているかの確認
+    print(train_list[0])
+    print("訓練画像の枚数: " + str(len(train_list)))
+    print(val_list[0])
+    print("検証画像の枚数: " + str(len(val_list)))
+
+    size = 224
+    mean = (0.485, 0.456, 0.406)
+    std = (0.229, 0.224, 0.225)
+    # データセットのサイズとラベルの確認
+    train_dataset = IsicDataset(
+        file_list=train_list, transform=ImageTransform(size, mean, std), phase="train"
+    )
+
+    val_dataset = IsicDataset(
+        file_list=val_list, transform=ImageTransform(size, mean, std), phase="val"
+    )
+
+    index = 0
+    print(val_dataset.__getitem__(index)[0].size())
+    print(val_dataset.__getitem__(index)[1])
--- usocky/main.py
+++ usocky/main.py
@@ -1,24 +1,20 @@
-
-import numpy as np
 import matplotlib.pyplot as plt
 
-import torch
 import torch.nn as nn
 import torch.optim as optim
 
-import torchvision
 from torchvision import models
 
 from ImageTransfom import ImageTransform
 from Dataset import IsicDataset, make_datapath_list, create_dataloader
 from Train_model import train_model
 
-#訓練データと検証データのパス
-train_list = make_datapath_list(phase = 'train')
-val_list = make_datapath_list(phase= 'val')
+# 訓練データと検証データのパス
+train_list = make_datapath_list(phase="train")
+val_list = make_datapath_list(phase="val")
 
-#画像表示と確認
-'''
+# 画像表示と確認
+"""
 size = 224
 mean = (0.485, 0.456, 0.406)
 std = (0.229, 0.224, 0.225)
@@ -34,50 +30,47 @@ img_transformed = img_transformed.numpy().transpose((1, 2, 0))
 img_transformed = np.clip(img_transformed, 0, 1)
 plt.imshow(img_transformed)
 plt.show()
-'''
+"""
 size = 224
 mean = (0.485, 0.456, 0.406)
 std = (0.229, 0.224, 0.225)
 
-#データセットの作成
+# データセットの作成
 train_dataset = IsicDataset(
-    file_list = train_list,
-    transform = ImageTransform(size, mean, std),
-    phase = 'train')
+    file_list=train_list, transform=ImageTransform(size, mean, std), phase="train"
+)
 
 val_dataset = IsicDataset(
-    file_list = val_list,
-    transform = ImageTransform(size, mean, std),
-    phase = 'val')
+    file_list=val_list, transform=ImageTransform(size, mean, std), phase="val"
+)
 
 
-#DataLoaderを作成
+# DataLoaderを作成
 batch_size = 32
 
-#辞書型'train'と'val'のバッチサイズandシャッフル
+# 辞書型'train'と'val'のバッチサイズandシャッフル
 dataloader_dict = create_dataloader(
-    batch_size = batch_size,
-    train_dataset = train_dataset,
-    val_dataset = val_dataset)
+    batch_size=batch_size, train_dataset=train_dataset, val_dataset=val_dataset
+)
 
-'''
+"""
 #バッチごとの動作確認
 batch_iterator = iter(dataloader_dict['val'])
 inputs, labels = next(batch_iterator)
 print(inputs.size())
 print(labels)
-'''
-#ネットワークモデルのロード
+"""
+# ネットワークモデルのロード
 use_pretrained = True
-net = models.resnet50(pretrained = use_pretrained)
+net = models.resnet50(pretrained=use_pretrained)
 
-net.fc = nn.Linear(in_features = 2048, out_features = 2, bias = True)
+net.fc = nn.Linear(in_features=2048, out_features=2, bias=True)
 net.train()
 
-#損失関数の設定
+# 損失関数の設定
 criterion = nn.CrossEntropyLoss()
 
-#調整するパラメータの設定
+# 調整するパラメータの設定
 params_to_update = []
 update_params_names = ["fc.weight", "fc.bias"]
 for name, param in net.named_parameters():
@@ -87,24 +80,24 @@ for name, param in net.named_parameters():
     else:
         param.requires_grad = False
 
-#最適化手法の設定
-optimizer = optim.SGD(params = params_to_update, lr = 0.001, momentum = 0.9)
+# 最適化手法の設定
+optimizer = optim.SGD(params=params_to_update, lr=0.001, momentum=0.9)
 
-#モデルの学習
+# モデルの学習
 num_epochs = 2
 history = train_model(net, dataloader_dict, criterion, optimizer, num_epochs)
 
-#学習履歴のグラフ化
+# 学習履歴のグラフ化
 plt.figure()
-plt.plot(range(1, num_epochs + 1, 1), history['train_loss'], label = 'train_loss')
-plt.plot(range(1, num_epochs + 1, 1), history['val_loss'], label = 'val_loss')
-plt.xlabel('epoch')
+plt.plot(range(1, num_epochs + 1, 1), history["train_loss"], label="train_loss")
+plt.plot(range(1, num_epochs + 1, 1), history["val_loss"], label="val_loss")
+plt.xlabel("epoch")
 plt.legend()
-plt.savefig('loss.png')
-    
+plt.savefig("loss.png")
+
 plt.figure()
-plt.plot(range(1, num_epochs + 1, 1), history['train_acc'], label = 'train_acc')
-plt.plot(range(1, num_epochs + 1, 1), history['val_acc'], label = 'val_acc')
-plt.xlabel('epoch')
+plt.plot(range(1, num_epochs + 1, 1), history["train_acc"], label="train_acc")
+plt.plot(range(1, num_epochs + 1, 1), history["val_acc"], label="val_acc")
+plt.xlabel("epoch")
 plt.legend()
-plt.savefig('acc.png')
+plt.savefig("acc.png")
